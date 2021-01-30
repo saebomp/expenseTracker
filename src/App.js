a18f4b1b71aa5a7fe576d0927ed64206a5002db0
SAEBOMPAK <saebompak@gmail.com>
2021-01-30 14:29:45 +0900

    add components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Header} from './components/Header'
+import {Balance} from './components/Balance'
+import {IncomeExpenses} from './components/IncomeExpenses'
+import {TransactionList} from './components/TransactionList'
+import {AddTransaction} from './components/AddTransaction'
+
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      hi
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
