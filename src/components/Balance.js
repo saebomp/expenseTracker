71e6151456ad043c61619be60efa6c0b90dccc43
SAEBOMPAK <saebompak@gmail.com>
2021-02-02 13:28:41 +0900

    installed tailwind

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -3,6 +3,7 @@ import {GlobalContext} from '../context/GlobalState'
 
 export const Balance = () => {
   const {transactions} = useContext(GlobalContext);
+  
   const amounts = transactions.map(transactions => transactions.amount);
   console.log('amounts', amounts)
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <h2>
+    <h2 className="uppercase text-purple-900">
       Expense Tracker
     </h2>
   )
